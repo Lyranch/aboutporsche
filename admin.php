c6d971a706de622b3086fac2964941a55fd7f03f
Lyranch <enoyeager@gmail.com>
2026-01-07 14:12:16 +0700

    ubah judul form login

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About Porsche | Admin</title>
+    <title>Welcome to About Porsche</title>
     <link rel="icon" type="image/png" href="img/logo.png">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
